66beda689416173594ec46eeb96281a4852f0bc4
B.M.B-TECH <hanchobmb@gmail.com>
2025-02-01 02:02:23 +0300

    Update Answer.js

--- commands/Answer.js
+++ commands/Answer.js
@@ -6,7 +6,7 @@ const { default: axios } = require('axios');
 
 
 
-zokou({nomCom:"BMB",reaction:"🙋",categorie:"IA"},async(dest,zk,commandeOptions)=>{
+zokou({nomCom:"bmb",reaction:"🙋",categorie:"IA"},async(dest,zk,commandeOptions)=>{
 
   const {repondre,ms,arg}=commandeOptions;
   
