ce3da7dafc11c0f2839cdfe005321b94ef17609f
B.M.B-TECH <hanchobmb@gmail.com>
2025-02-04 16:46:26 +0300

    Add files via upload

---
+++ commands/bmbhf.js
@@ -0,0 +1,218 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  'value': true
+});
+const {
+  zokou
+} = require("../framework/zokou");
+zokou({
+  'nomCom': "wagroup",
+  'reaction': '🤔',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x3258e7, _0x4c4732, _0x13b70c) => {
+  console.log("Commande saisie !!!s");
+  await _0x4c4732.sendMessage(_0x3258e7, {
+    'text': "Hello 👋\n\nClick on the button below to join the OFFICIAL *B.M.B-MD* WhatsApp Group",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': "https://chat.whatsapp.com/GSwWooIVuAa3VZg095sTux",
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/iz2ds9.jpg",
+        'title': "Join Our WhatsApp Group",
+        'body': "Click to join the official LUCKY-MD WhatsApp group!"
+      }
+    }
+  });
+  console.log("Command executed: wagroup");
+});
+zokou({
+  'nomCom': 'wachannel',
+  'reaction': '🫴',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the button below to Follow the OFFICIAL *B.M.B-MD* WhatsApp Channel",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://whatsapp.com/channel/0029VaoadqE84OmC8xlVsQ1M',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/quje1n.webp",
+        'title': "Join Our WhatsApp Channel",
+        'body': "Click to join the official B.M.B-MD WhatsApp channel!"
+      }
+    }
+  });
+  console.log("Command executed: wachannel");
+});
+zokou({
+  'nomCom': 'waowner',
+  'reaction': '🫴',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the button below to contact the OFFICIAL *B.M.B-MD* Owner",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https:// wa.me/255711782669',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/56bjny.jpg",
+        'title': "Join Our Developer Place",
+        'body': "Click to join the official B.M.B-MD Owner Inbox!"
+      }
+    }
+  });
+  console.log("Command executed: waowner");
+});
+zokou({
+  'nomCom': 'fb-page',
+  'reaction': '🫴',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the photo below to Follow the OFFICIAL *BMBTECH* Facebook Page",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://www.facebook.com/profile.php?id=100091667352937',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/iz2ds9.jpg",
+        'title': "Follow Facebook Page 📄",
+        'body': "Click to join the official BMBTECH Facebook Page!"
+      }
+    }
+  });
+  console.log("Command executed: fb-page");
+});
+zokou({
+  'nomCom': 'insta-page',
+  'reaction': '🫴',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the photo below to Follow the OFFICIAL *B.M.BTECH* Instagram Page",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://www.facebook.com/profile.php?id=100091667352937',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/56bjny.jpg",
+        'title': "Follow Instagram Page 📄",
+        'body': "Click to join the official B-M-B TECH Instagram Page!"
+      }
+    }
+  });
+  console.log("Command executed: insta-page");
+});
+zokou({
+  'nomCom': 'threads-page',
+  'reaction': '🫴',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the photo below to Follow the OFFICIAL *B.M.BTECH* Threads Page",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://chat.whatsapp.com/GSwWooIVuAa3VZg095sTux',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/iz2ds9.jpg",
+        'title': "Follow Threads Page 📄",
+        'body': "Click to join the official B.M.BTECH Threads Page!"
+      }
+    }
+  });
+  console.log("Command executed: threads-page");
+});
+zokou({
+  'nomCom': 'tiktok-page',
+  'reaction': '🫴',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the photo below to Follow the OFFICIAL *B-M-B TECH* TikTok Page",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://vm.tiktok.com/ZMk4F72Ey/',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/56bjny.jpg",
+        'title': "Follow TikTok Page 📄",
+        'body': "Click to join the official FREDIETECH TikTok Page!"
+      }
+    }
+  });
+  console.log("Command executed: tiktok-page");
+});
+zokou({
+  'nomCom': 'tgroup',
+  'reaction': '🫴',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the photo below to Join the OFFICIAL *B.M.B_MD* Telegram Group",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://wa.me/p/9846531168691219/255711782669',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/iz2ds9.jpg",
+        'title': "Join Telegram Group📄",
+        'body': "Click to join the official B.M.B_MD Telegram Group!"
+      }
+    }
+  });
+  console.log("Command executed: tgroup");
+});
+zokou({
+  'nomCom': 'ytchannel',
+  'reaction': '🫴',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the photo below to Subscribe the OFFICIAL *B.M.BTECH* YouTube Channel",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://youtube.com/@bmbtech-tz?si=MN6XdFPmSXsJC1sj,
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/7irwqn.jpeg",
+        'title': "Follow YouTube Channel📄",
+        'body': "Click to Subscribe the official B.M.BTECH YouTube Channel!"
+      }
+    }
+  });
+  console.log("Command executed: ytchannel");
+});
+zokou({
+  'nomCom': 't-help',
+  'reaction': '🫴',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the photo below to connect the OFFICIAL *B.M.BTECH* Telegram Inbox",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://wa.me/p/9846531168691219/255711782669',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/56bjny.jpg",
+        'title': "Chating With Owner",
+        'body': "Click to Contact the official B.M.BTECH Telegram Inbox!"
+      }
+    }
+  });
+  console.log("Command executed: t-help");
+});
